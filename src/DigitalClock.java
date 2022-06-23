f423ed2275bf79d43c55524f7a82164c841f7eab
jsilence82 <jeff.silence@gmail.com>
2022-06-23 16:17:59 +0200

    Updated time zones

--- src/DigitalClock.java
+++ src/DigitalClock.java
@@ -1,25 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class DigitalClock {
+
     static void display(){
         JFrame clockFrame = new JFrame("The Current Time Is");
         clockFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        clockFrame.setPreferredSize(new Dimension(400, 200));
+        clockFrame.setPreferredSize(new Dimension(600, 200));
 
         JLabel currentTimeLabel = new JLabel("", SwingConstants.CENTER);
         currentTimeLabel.setPreferredSize(new Dimension(400, 100));
         currentTimeLabel.setFont(new Font("Arial", Font.BOLD,50));
         clockFrame.getContentPane().add(currentTimeLabel, BorderLayout.NORTH);
 
+        String[] timeZonesChoice = {"UTC", "PST", "MST", "CST", "EST", "CEST"};
+        JComboBox<String> jComboBox = new JComboBox<>(timeZonesChoice);
+        jComboBox.setPreferredSize(new Dimension(5, 10));
+        jComboBox.setSelectedIndex(0);
+        clockFrame.getContentPane().add(jComboBox);
+
         JLabel uTCTimeLabel = new JLabel("", SwingConstants.CENTER);
-        uTCTimeLabel.setPreferredSize(new Dimension(400, 100));
+        uTCTimeLabel.setPreferredSize(new Dimension(500, 50));
         uTCTimeLabel.setFont(new Font("Arial", Font.BOLD,50));
-        clockFrame.getContentPane().add(uTCTimeLabel, BorderLayout.SOUTH);
+        clockFrame.getContentPane().add(uTCTimeLabel, BorderLayout.EAST);
 
         clockFrame.setLocationRelativeTo(null);
         clockFrame.pack();
@@ -32,14 +42,31 @@ public class DigitalClock {
             String formattedTime = current.format(formatted);
             currentTimeLabel.setText("Local: " + formattedTime);
         });
-        Timer uTCTimer = new Timer(delay, e -> {
-            OffsetDateTime current = OffsetDateTime.now(ZoneOffset.UTC);
-            DateTimeFormatter formatted = DateTimeFormatter.ofPattern("HH:mm:ss");
-            String formattedUTC = current.format(formatted);
-            uTCTimeLabel.setText("UTC: " + formattedUTC);
-        });
         currentTimer.start();
-        uTCTimer.start();
+
+        jComboBox.addActionListener(e -> {
+            Timer uTCTimer = new Timer(delay, i -> {
+                String timeZone = jComboBox.getItemAt(jComboBox.getSelectedIndex());
+                OffsetDateTime current;
+            if (Objects.equals(timeZone, "PST")) {
+                 current = OffsetDateTime.now(ZoneOffset.of("-08:00"));
+            } else if (Objects.equals(timeZone,"MST")) {
+                current = OffsetDateTime.now(ZoneOffset.of("-07:00"));
+            } else if (Objects.equals(timeZone, "CST")) {
+                current = OffsetDateTime.now(ZoneOffset.of("-06:00"));
+            } else if (Objects.equals(timeZone, "EST")) {
+                current = OffsetDateTime.now(ZoneOffset.of("-05:00"));
+            } else if (Objects.equals(timeZone, "CEST")) {
+                current = OffsetDateTime.now(ZoneOffset.of("+02:00"));
+            } else {
+                current = OffsetDateTime.now(ZoneOffset.UTC);
+            }
+            DateTimeFormatter formatted = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String formattedTimeZone = current.format(formatted);
+            uTCTimeLabel.setText(formattedTimeZone);
+            });
+            uTCTimer.start();
+            });
     }
 
     public static void main(String[] args){
