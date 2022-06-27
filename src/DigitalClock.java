e320dbd09d64e6e7d2137466821017d36eb7c6e0
jsilence82 <jeff.silence@gmail.com>
2022-06-27 09:53:03 +0200

    Deleted unnecessary imports

--- src/DigitalClock.java
+++ src/DigitalClock.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
