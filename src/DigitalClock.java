273c4f871380fac16bccd3113bbd39aef7121df8
Jeff Silence <98595145+jsilence82@users.noreply.github.com>
2022-11-28 15:20:14 +0100

    Updated layout

--- src/DigitalClock.java
+++ src/DigitalClock.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
@@ -6,39 +7,67 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.TimeZone;
 
-public class DigitalClock {
+public class DigitalClock extends JFrame {
 
-    static void display(){
-        JFrame clockFrame = new JFrame("The Current Time Is");
-        clockFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        clockFrame.setPreferredSize(new Dimension(600, 200));
+    public DigitalClock() {
+        setTitle("The Time is...");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 500, 200);
+
+        JPanel contentPanel = new JPanel();
+        contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        setContentPane(contentPanel);
+        GridBagLayout gbl_contentPane = new GridBagLayout();
+        gbl_contentPane.columnWidths = new int[]{141, 141, 0};
+        gbl_contentPane.rowHeights = new int[]{48, 46, 0};
+        gbl_contentPane.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+        gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+        contentPanel.setLayout(gbl_contentPane);
+
+        JLabel lblNewLabel = new JLabel("Local Time", SwingConstants.CENTER);
+        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 17));
+        GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+        gbc_lblNewLabel.fill = GridBagConstraints.BOTH;
+        gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel.gridx = 0;
+        gbc_lblNewLabel.gridy = 1;
+        contentPanel.add(lblNewLabel, gbc_lblNewLabel);
+
 
         JLabel currentTimeLabel = new JLabel("", SwingConstants.CENTER);
-        currentTimeLabel.setPreferredSize(new Dimension(400, 100));
-        currentTimeLabel.setFont(new Font("Arial", Font.BOLD,50));
-        clockFrame.getContentPane().add(currentTimeLabel, BorderLayout.NORTH);
+        currentTimeLabel.setFont(new Font("Tahoma", Font.BOLD, 17));
+        GridBagConstraints gbc_local = new GridBagConstraints();
+        gbc_local.fill = GridBagConstraints.BOTH;
+        gbc_local.insets = new Insets(0, 0, 5, 0);
+        gbc_local.gridx = 1;
+        gbc_local.gridy = 1;
+        contentPanel.add(currentTimeLabel, gbc_local);
+
 
         String[] timeZonesChoice = TimeZone.getAvailableIDs();
         JComboBox<String> jComboBox = new JComboBox<>(timeZonesChoice);
-        jComboBox.setPreferredSize(new Dimension(5, 10));
-        jComboBox.setSelectedIndex(0);
-        clockFrame.getContentPane().add(jComboBox);
+        GridBagConstraints gbc_comboBox = new GridBagConstraints();
+        gbc_comboBox.insets = new Insets(0, 0, 0, 5);
+        gbc_comboBox.fill = GridBagConstraints.BOTH;
+        gbc_comboBox.gridx = 0;
+        gbc_comboBox.gridy = 2;
+        contentPanel.add(jComboBox, gbc_comboBox);
+
 
         JLabel uTCTimeLabel = new JLabel("", SwingConstants.CENTER);
-        uTCTimeLabel.setPreferredSize(new Dimension(500, 50));
-        uTCTimeLabel.setFont(new Font("Arial", Font.BOLD,50));
-        clockFrame.getContentPane().add(uTCTimeLabel, BorderLayout.EAST);
-
-        clockFrame.setLocationRelativeTo(null);
-        clockFrame.pack();
-        clockFrame.setVisible(true);
+        uTCTimeLabel.setFont(new Font("Tahoma", Font.BOLD, 17));
+        GridBagConstraints gbc_utcTimelablel = new GridBagConstraints();
+        gbc_utcTimelablel.gridx = 1;
+        gbc_utcTimelablel.gridy = 2;
+        contentPanel.add(uTCTimeLabel, gbc_utcTimelablel);
 
         int delay = 100;
         Timer currentTimer = new Timer(delay, e -> {
             LocalDateTime current = LocalDateTime.now();
             DateTimeFormatter formatted = DateTimeFormatter.ofPattern("HH:mm:ss");
             String formattedTime = current.format(formatted);
-            currentTimeLabel.setText("Local: " + formattedTime);
+            currentTimeLabel.setText(formattedTime);
         });
         currentTimer.start();
 
@@ -46,19 +75,26 @@ public class DigitalClock {
             Timer uTCTimer = new Timer(delay, i -> {
                 String timeZone = jComboBox.getItemAt(jComboBox.getSelectedIndex());
                 TimeZone zone = TimeZone.getTimeZone(timeZone);
-                int offset = zone.getRawOffset()/1000;
-                int hour = offset/3600;
-                int minutes = (offset % 3600)/60;
-                OffsetDateTime current = OffsetDateTime.now(ZoneOffset.ofHoursMinutes(hour,minutes));
+                int offset = zone.getRawOffset() / 1000;
+                int hour = offset / 3600;
+                int minutes = (offset % 3600) / 60;
+                OffsetDateTime current = OffsetDateTime.now(ZoneOffset.ofHoursMinutes(hour, minutes));
                 DateTimeFormatter formatted = DateTimeFormatter.ofPattern("HH:mm:ss");
                 String formattedTimeZone = current.format(formatted);
                 uTCTimeLabel.setText(formattedTimeZone);
             });
             uTCTimer.start();
-            });
+        });
     }
 
-    public static void main(String[] args){
-        display();
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                DigitalClock frame = new DigitalClock();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
---
+++ src/Viewer.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Viewer {
+    private JComboBox comboTimeZone;
+    private JLabel localLabel;
+    private JLabel zoneTimeLabel;
+}
